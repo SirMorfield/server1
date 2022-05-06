096ca60f3642d515db35cfc361746948985d2228
SirMorfield <jkoers@student.codam.nl>
2022-05-06 15:30:07 +0200

    feat: now always install docker_compose latest version

--- preinstall.sh
+++ preinstall.sh
@@ -6,15 +6,14 @@ set -o nounset
 IFS=$(printf '\n\t')
 
 # Docker
-# sudo apt remove --yes docker docker-engine docker.io containerd runc
+sudo apt remove --yes docker docker-engine docker.io containerd runc || true
 sudo apt update
 sudo apt --yes --no-install-recommends install apt-transport-https ca-certificates
 wget --quiet --output-document=- https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
+sudo add-apt-repository --yes "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
 sudo apt update
 sudo apt --yes --no-install-recommends install docker-ce docker-ce-cli containerd.io
 sudo usermod --append --groups docker "$USER"
-sleep 5
 sudo systemctl enable docker
 printf '\nDocker installed successfully\n\n'
 
@@ -22,17 +21,6 @@ printf 'Waiting for Docker to start...\n\n'
 sleep 10
 
 # Docker Compose
-sudo apt install curl -y
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+LATEST_VERSION=$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')
+sudo curl -L "https://github.com/docker/compose/releases/download/$LATEST_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -f -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-sudo groupadd -f docker
-sudo usermod -aG docker $USER
-newgrp docker
-docker run hello-world --rm
-sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
-sudo chmod g+rwx "$HOME/.docker" -R
-
-printf '\nDocker Compose installed successfully'
-docker-compose --version
