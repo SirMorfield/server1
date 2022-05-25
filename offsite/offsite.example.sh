3c648a350d4434276a88ecb5d5104422881b6320
SirMorfield <joppe.koers@gmail.com>
2022-05-25 22:55:19 +0000

    feat: store secrets bett

--- offsite/offsite.example.sh
+++ offsite/offsite.example.sh
@@ -14,7 +14,7 @@ BACKUP_DIRS+=$(cd $HOME && find files/ -maxdepth 1 \
 echo $BACKUP_DIRS
 
 BACKUP_DIRS+="'server1/runtimeGenerated'"
-SERVER="root@192.168.2.21"
+SERVER="root@server"
 SSHCMD="ssh"
 
 REMOTE_MOUNT_PATH="/root/files"
@@ -24,11 +24,11 @@ REMOTE_LUKS_NAME="volume1"
 # TODO: shutdown and remote wake on lan
 
 $SSHCMD $SERVER mkdir -p $REMOTE_MOUNT_PATH
-$SSHCMD $SERVER echo '' '|' cryptsetup luksOpen /dev/disk/by-uuid/5b328bfd-528f-4e46-9974-5f86ca196011 volume1 -d=- || true
+$SSHCMD $SERVER echo 'INJECT_DISK_PASSWORD' '|' cryptsetup luksOpen /dev/disk/by-uuid/5b328bfd-528f-4e46-9974-5f86ca196011 volume1 -d=- || true
 $SSHCMD $SERVER mount /dev/mapper/$REMOTE_LUKS_NAME $REMOTE_MOUNT_PATH || true
 
 # TODO: enable compression?, verify disk is mounted
-sh -c "rsync --archive --no-links --human-readable -P --one-file-system --delete-after --delete-excluded -e '$SSHCMD' $BACKUP_DIRS $SERVER:$REMOTE_MOUNT_PATH"
+sh -c "rsync --archive --no-links --human-readable -P --one-file-system --delete-after --delete-excluded -e '$SSHCMD' $BACKUP_DIRS $SERVER:$REMOTE_MOUNT_PATH"  || true
 
 $SSHCMD $SERVER umount $REMOTE_MOUNT_PATH
 $SSHCMD $SERVER cryptsetup luksClose /dev/mapper/$REMOTE_LUKS_NAME
