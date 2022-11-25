d583b4de3214c5e42ee80f0b46ec0493d2190eff
Joppe Koers <joppe@joppekoers.nl>
2022-11-25 16:33:59 +0000

    chore: clean up monitors

--- monitor/src/app.ts
+++ monitor/src/app.ts
@@ -1,40 +1,16 @@
-import fetch from 'node-fetch'
 import dotenv from 'dotenv'
 import util from 'util'
+import { getFindPeersAlert } from './monitors'
 
 dotenv.config()
 import { sendMail } from './mailer' // import only after injecting .env vars
 
 util.inspect.defaultOptions.depth = 10
 
-interface Alert {
+export interface Alert {
 	date: Date
 	type: string // unique key that lets the program know what type of alert it is and not have to send it every time it triggers.
 	message: string
-}
-
-async function getFindPeersAlert(): Promise<undefined | Alert> {
-	const maxSyncHours = 35 // maximum time since last sync
-
-	try {
-		const response = await fetch('https://find-peers.codam.nl/status/pull')
-		const json = await response.json() as { name: string, lastPull: Date, ago: { ms: number, human: string } }[]
-		const alerts = json.filter((x) => x.ago.ms > maxSyncHours * 60 * 60 * 1000)
-		if (alerts.length === 0) {
-			return undefined
-		}
-		return {
-			date: new Date(),
-			type: 'FindPeers timeout',
-			message: `Sync took more than ${maxSyncHours} hours\n ${JSON.stringify(alerts, null, 4)}`
-		}
-	} catch (e) {
-		return {
-			date: new Date(),
-			type: 'FindPeers internal error',
-			message: `Error while fetching FindPeers status: ${e}`
-		}
-	}
 }
 
 let sendAlerts: { type: string, date: Date }[] = []
@@ -61,6 +37,6 @@ let sendAlerts: { type: string, date: Date }[] = []
 			await sendMail(process.env['RECEIVER_EMAIL'] || '', content, `${alerts.length.toString().padStart(2)}  Monitoring alerts`)
 		}
 
-		await new Promise(resolve => setTimeout(resolve, 60 * 1000)) // TODO check mor often, but don't send too many emails
+		await new Promise(resolve => setTimeout(resolve, 60 * 1000))
 	}
 })()
