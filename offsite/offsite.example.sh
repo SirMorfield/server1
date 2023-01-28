b0166f2b092b56cba2b7aef8dc7a73e9765d897c
Joppe Koers <joppe@joppekoers.nl>
2023-01-28 17:43:27 +0100

    Improve remote backup

--- offsite/offsite.example.sh
+++ offsite/offsite.example.sh
@@ -3,26 +3,27 @@
 set -e # quit on error
 set -x # print exectued commands
 
-BACKUP_DIRS+="$HOME/files "
-BACKUP_DIRS+="$HOME/server1/runtimeGenerated "
-BACKUP_DIRS+="$HOME/git "
-echo $BACKUP_DIRS
-
-SERVER="root@server"
-SSHCMD="ssh"
-
+HOME="/home/joppe"
+BACKUP_DIR="$HOME/files"
+SERVER="root@87.214.63.99"
+SSHCMD="ssh -p 10001"
+REMOTE_MOUNT_PASSWORD="password"
 REMOTE_MOUNT_PATH="/root/files"
 REMOTE_DRIVE_UUID="5b328bfd-528f-4e46-9974-5f86ca196011" # Use blkid to find this
 REMOTE_LUKS_NAME="volume1"
 
-# TODO: shutdown and remote wake on lan
+RSYNC="rsync -z --archive --no-links --human-readable -P --one-file-system --delete-excluded"
 
 $SSHCMD $SERVER mkdir -p $REMOTE_MOUNT_PATH
-$SSHCMD $SERVER echo 'INJECT_DISK_PASSWORD' '|' cryptsetup luksOpen /dev/disk/by-uuid/$REMOTE_DRIVE_UUID volume1 -d=- || true
+$SSHCMD $SERVER echo "$REMOTE_MOUNT_PASSWORD" '|' cryptsetup luksOpen /dev/disk/by-uuid/$REMOTE_DRIVE_UUID volume1 -d=- || true
 $SSHCMD $SERVER mount /dev/mapper/$REMOTE_LUKS_NAME $REMOTE_MOUNT_PATH || true
 
-# TODO: enable compression?, verify disk is mounted
-sh -c "rsync -z --archive --no-links --human-readable -P --one-file-system --delete-excluded -e '$SSHCMD' $BACKUP_DIRS $SERVER:$REMOTE_MOUNT_PATH" || true
+# Copy ssd folders to big hdd
+$RSYNC "$HOME/server1/runtimeGenerated" "$BACKUP_DIR/sync/server1"
+$RSYNC "$HOME/git" "$BACKUP_DIR/sync"
+
+# TODO: verify disk is mounted
+$RSYNC -e "$SSHCMD" "$BACKUP_DIR" "$SERVER:$REMOTE_MOUNT_PATH" || true
 
 $SSHCMD $SERVER umount $REMOTE_MOUNT_PATH
 $SSHCMD $SERVER cryptsetup luksClose /dev/mapper/$REMOTE_LUKS_NAME
