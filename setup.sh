eab6ab37705bfd4170b9138bb7229bd6d1887fab
Joppe Koers <joppe@joppekoers.nl>
2023-01-30 14:08:49 +0000

    Add setup

--- setup.sh
+++ setup.sh
@@ -7,3 +7,6 @@ echo "/dev/disk/by-uuid/13b9900e-3d41-4793-9b41-3f6441dc6b41 /home/joppe/files e
 # install crontab
 
 echo "0 3 */2 * * /bin/bash /home/joppe/server1/offsite/offsite.sh | grep -v 'skipping non-regular file' &>> /home/joppe/offsite.txt ; echo $(date) >> /home/joppe/offsite.txt" | crontab -
+
+curl -sL https://deb.nodesource.com/setup_18.x | sudo bash -
+sudo apt-get install -y nodejs
